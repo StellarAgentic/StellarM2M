dbda5f26878e15554d23d8e9b416bc847cb4a4e0
Davoski1 <adeoladavid176@gmail.com>
2026-09-13 00:38:56 +0100

    feat: interceptor detects 402 HTTP status codes

--- src/stellar_m2m/interceptor.py
+++ src/stellar_m2m/interceptor.py
@@ -2,16 +2,16 @@ import httpx
 import typing 
 
 class PaywallInterceptor(httpx.Auth):
-    """"""
+    """
     Middleware to intercept outgoing HTTP requests made by our AI Agent.
 
     Future Purpose :
     This class will detect '402 Payment Required' responses from merchants, extract the required payment amount and 
     destination, trigger the AgentWallet to make an on-chain stellar payment, and then automatically retry the request.
-    """"""
+    """
 
-    def auth_flow(self, httpx.Request) -> typing.Generator[httpx.Request, httpx.Response, None]:
-        #For now we do nothing except pass the request through. 
-
-        yield request
-    
+    def auth_flow(self, request: httpx.Request) -> typing.Generator[httpx.Request, httpx.Response, None]:
+        response = yield request
+        
+        if response.status_code == 402:
+            print("402 Paywall detected!")
